d899d998f9199a897bb0b05ede4135c98134c43e
Marielly Sales <mariellysales01@icloud.com>
2023-06-05 10:35:23 -0300

    Atributo e método público

---
+++ Funcionario.php
@@ -0,0 +1,16 @@
+<?php
+class Funcionario
+{
+    public string $nome;
+    public float $salario;
+
+    public function verSalario(): string
+    {
+        return "O(a) funcionário(a) {$this->nome} tem o salário R$ {$this->converterSalario($this->salario)}";
+    }
+
+    public function converterSalario(): string
+    {
+        return number_format($this->salario, '2', ',', '.');
+    }
+}
--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
-    <title>Atributo estático</title>
+    <meta charset="UTF-8">
+    <title>Salário</title>
 </head>
 
 <body>
     <?php
-    require './Disciplina.php';
+    require "./Funcionario.php";
+    $funcionario = new Funcionario();
+    $funcionario->nome = "Geovana";
+    $funcionario->salario = 7961.75;
+    echo $funcionario->verSalario();
 
-    //Acessa o atributo sem criar o objeto
-    echo "Média: " . Disciplina::$media . "<br><hr>";
-
-    $disciplina = new Disciplina("Aline", 3, 5);
-    echo $disciplina->situacao();
-
-    echo Disciplina::situacaoAluno(9);
     ?>
-
 </body>
 
 </html>
