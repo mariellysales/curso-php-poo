6dd0593a9c151444e93e82f490ddbf29796ca21d
Marielly Sales <mariellysales01@icloud.com>
2023-07-19 10:54:36 -0300

    Usando SELECT com MySQL e PHP

--- index.php
+++ index.php
@@ -3,17 +3,68 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Select</title>
+    <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 
 <body>
     <?php
-    //responsável por carregar automaticamente as classes e arquivos do composer
-    require "./vendor/autoload.php";
-    //Instanciando a classe
-    $url = new Core\ConfigController();
-    //Chama o método para carregar a página
-    $url->loadPage();
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "cadastro";
+    $port = 3306;
+
+    try {
+        $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+        echo "Conexão com banco de dados realizada com sucesso!<br>";
+    } catch (Exception $err) {
+
+        echo "ERRO: Conexão com banco de dados não realizada. Erro gerado: " . $err->getMessage();
+    }
+
+    echo "<h2>Listar usuários</h2>";
+    //A query com "*" indica que deve trazer todas as colunas
+    $query_usuarios = "SELECT * FROM usuarios";
+    $result_usuarios = $conn->prepare($query_usuarios);
+    $result_usuarios->execute();
+
+    while ($row_usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)) {
+        echo "ID: " . $row_usuario['id'] . "<br>";
+        echo "Nome: " . $row_usuario['nome'] . "<br>";
+        echo "Email: " . $row_usuario['email'] . "<br>";
+        echo "Cadastrado em: " . date('d/m/Y H:i:s', strtotime($row_usuario['created'])) . "<br>";
+        echo "Editado: ";
+        if (!empty($row_usuario['modified'])) {
+            echo "Editado: " . date('d/m/Y H:i:s', strtotime($row_usuario['modified'])) . "<br>";
+        }
+        echo "<br>";
+
+        echo "<hr>";
+    }
+
+    echo "<h2>Listar usuários otimizado</h2>";
+    //Indica quais colunas devem retornar o valor
+    $query_usuarios_b = "SELECT id, nome, email, created, modified FROM usuarios";
+    $result_usuarios_b = $conn->prepare($query_usuarios);
+    $result_usuarios_b->execute();
+
+    while ($row_usuario_b = $result_usuarios_b->fetch(PDO::FETCH_ASSOC)) {
+        extract($row_usuario_b);
+        echo "ID: $id <br>";
+        echo "Nome: $nome <br>";
+        echo "Email: $email <br>";
+        echo "Cadastrado em: " . date('d/m/Y H:i:s', strtotime($created)) . "<br>";
+        echo "Editado: ";
+        if (!empty($modified)) {
+            echo "Editado: " . date('d/m/Y H:i:s', strtotime($modified)) . "<br>";
+        }
+        echo "<br>";
+
+        echo "<hr>";
+    }
+
     ?>
 </body>
 
