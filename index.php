be8a22064841903c9e769dbc7c9cf7a0d223d241
Marielly Sales <mariellysales01@icloud.com>
2023-07-19 18:05:35 -0300

    ORDER BY no MySQL

--- index.php
+++ index.php
@@ -6,14 +6,15 @@ include_once "conexao.php";
 
 <head>
     <meta charset="UTF-8">
-    <title>AND e OR</title>
+    <title>ORDER BY</title>
     <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 
 <body>
-    <h2>Visualizar usuários com duas condições usando o AND</h2>
+    <h2>Listar usuários</h2>
+
     <?php
-    $query_usuario = "SELECT id, nome, email, sits_usuario_id, niveis_acesso_id FROM usuarios WHERE sits_usuario_id = 3 AND niveis_acesso_id = 3 LIMIT 10";
+    $query_usuario = "SELECT id, nome, email, sits_usuario_id, niveis_acesso_id FROM usuarios ORDER BY nome ASC";
     $result_usuario = $conn->prepare($query_usuario);
     $result_usuario->execute();
 
@@ -28,39 +29,7 @@ include_once "conexao.php";
         echo "Id do nível de acesso: $niveis_acesso_id <br>";
         echo "<hr>";
     }
+    ?>
+</body>
 
-    echo "<h2>Visualizar usuários com duas condições usando o OR</h2>";
-
-    $query_usuario_b = "SELECT id, nome, email, sits_usuario_id, niveis_acesso_id FROM usuarios WHERE sits_usuario_id = 3 OR niveis_acesso_id = 1 LIMIT 10";
-    $result_usuario_b = $conn->prepare($query_usuario_b);
-    $result_usuario_b->execute();
-
-    while ($row_usuario_b = $result_usuario_b->fetch(PDO::FETCH_ASSOC)) {
-        //imprimir através da chave do array
-        extract($row_usuario_b);
-
-        echo "ID: $id <br>";
-        echo "Nome: $nome <br>";
-        echo "E-mail: $email <br>";
-        echo "Id da situação: $sits_usuario_id <br>";
-        echo "Id do nível de acesso: $niveis_acesso_id <br>";
-        echo "<hr>";
-    }
-
-    echo "<h2>Visualizar usuários com duas condições usando o AND e OR</h2>";
-
-    $query_usuario_c = "SELECT id, nome, email, sits_usuario_id, niveis_acesso_id FROM usuarios WHERE (sits_usuario_id = 1 AND niveis_acesso_id = 2) OR (sits_usuario_id = 2) LIMIT 10";
-    $result_usuario_c = $conn->prepare($query_usuario_c);
-    $result_usuario_c->execute();
-
-    while ($row_usuario_c = $result_usuario_c->fetch(PDO::FETCH_ASSOC)) {
-        //imprimir através da chave do array
-        extract($row_usuario_c);
-
-        echo "ID: $id <br>";
-        echo "Nome: $nome <br>";
-        echo "E-mail: $email <br>";
-        echo "Id da situação: $sits_usuario_id <br>";
-        echo "Id do nível de acesso: $niveis_acesso_id <br>";
-        echo "<hr>";
-    }
+</html>
