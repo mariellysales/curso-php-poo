360681c5f4dd38c6b1a3c46e853fd130879fddbb
Marielly Sales <mariellysales01@icloud.com>
2023-07-21 15:25:11 -0300

    Mantendo dados no formulário após erro de cadastro

--- index.php
+++ index.php
@@ -27,37 +27,74 @@ include_once "conexao.php";
     $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
     if (!empty($dados['SendCadUsuario'])) {
-        $query_usuario = "INSERT INTO usuarios (nome, email, senha, sists_usuario_id, niveis_acesso_id, created) VALUES (:nome, :email, :senha, :sists_usuario_id, :niveis_acesso_id, NOW())";
-        $cad_usuario = $conn->prepare($query_usuario);
-        $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
-        $cad_usuario->bindParam(':email', $dados['email']);
-        //criptografar senha
-        $senha_cript = password_hash($dados['senha'], PASSWORD_DEFAULT);
-        $cad_usuario->bindParam(':senha', $senha_cript);
-        $cad_usuario->bindParam(':sists_usuario_id', $dados['sists_usuario_id'], PDO::PARAM_INT);
-        $cad_usuario->bindParam(':niveis_acesso_id', $dados['niveis_acesso_id'], PDO::PARAM_INT);
-        $cad_usuario->execute();
 
-        if ($cad_usuario->rowCount()) {
-            echo "Usuário cadastrado com sucesso!";
-            //Como recuperar id do registro cadastrado
-            //echo $conn->lastInsertId();
-        } else {
-            echo "ERRO: Usuário não cadastrado";
+        try {
+            $query_usuario = "INSERT INTO usuarios (nome, email, senha, sists_usuario_id, niveis_acesso_id, created) VALUES (:nome, :email, :senha, :sists_usuario_id, :niveis_acesso_id, NOW())";
+            $cad_usuario = $conn->prepare($query_usuario);
+            $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
+            $cad_usuario->bindParam(':email', $dados['email']);
+            //criptografar senha
+            $senha_cript = password_hash($dados['senha'], PASSWORD_DEFAULT);
+            $cad_usuario->bindParam(':senha', $senha_cript);
+            $cad_usuario->bindParam(':sists_usuario_id', $dados['sists_usuario_id'], PDO::PARAM_INT);
+            $cad_usuario->bindParam(':niveis_acesso_id', $dados['niveis_acesso_id'], PDO::PARAM_INT);
+            $cad_usuario->execute();
+
+            if ($cad_usuario->rowCount()) {
+                echo "Usuário cadastrado com sucesso!";
+                //destroi os dados mantidos no formulário
+                unset($dados);
+            } else {
+                echo "ERRO: Usuário não cadastrado";
+            }
+        } catch (PDOException $erro) {
+            echo "ERRO: Usuário não cadastrado.";
+            //echo "ERRO: Usuário não cadastrado. Erro gerado: " . $erro->getMessage() . "<br>";
         }
     }
     ?>
     <form method="POST" action="">
         <label>Nome:</label>
-        <input type="text" name="nome" placeholder="Nome completo" required /><br><br>
+        <!-- Mantendo dados no formulário:  -->
+        <?php
+        $nome = "";
+        if (isset($dados['nome'])) {
+            $nome = $dados['nome'];
+        }
+        ?>
+        <input type="text" name="nome" placeholder="Nome completo" value="<?php echo $nome; ?>" required /><br><br>
         <label>E-mail:</label>
-        <input type="email" name="email" placeholder="E-mail do usuário" required /><br><br>
+        <?php
+        $email = "";
+        if (isset($dados['email'])) {
+            $email = $dados['email'];
+        }
+        ?>
+        <input type="email" name="email" placeholder="E-mail do usuário" value="<?php echo $email; ?>" required /><br><br>
         <label>Senha:</label>
-        <input type="password" name="senha" placeholder="Senha do usuário" required /><br><br>
+        <?php
+        $senha = "";
+        if (isset($dados['senha'])) {
+            $senha = $dados['senha'];
+        }
+        ?>
+        <input type="password" name="senha" placeholder="Senha do usuário" value="<?php echo $senha; ?>" required /><br><br>
         <label>Situação:</label>
-        <input type="number" name="sists_usuario_id" placeholder="Situação do usuário" required /><br><br>
+        <?php
+        $situacao = "";
+        if (isset($dados['sists_usuario_id'])) {
+            $situacao = $dados['sists_usuario_id'];
+        }
+        ?>
+        <input type="number" name="sists_usuario_id" placeholder="Situação do usuário" value="<?php echo $situacao; ?>" required /><br><br>
         <label>Nível de acecsso:</label>
-        <input type="number" name="niveis_acesso_id" placeholder="Nível de acecsso do usuário" required /><br><br>
+        <?php
+        $nivel_acesso = "";
+        if (isset($dados['niveis_acesso_id'])) {
+            $nivel_acesso = $dados['niveis_acesso_id'];
+        }
+        ?>
+        <input type="number" name="niveis_acesso_id" placeholder="Nível de acecsso do usuário" value="<?php echo $nivel_acesso; ?>" required /><br><br>
         <input type="submit" value="Cadastrar" name="SendCadUsuario" />
 
     </form>
