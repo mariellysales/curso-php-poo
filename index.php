9f5a9f87f53eaa05b4d4386f2a2c1033db10b4c4
Marielly Sales <mariellysales01@icloud.com>
2023-07-21 21:06:29 -0300

    UPDATE no MySQL

--- index.php
+++ index.php
@@ -6,126 +6,30 @@ include_once "conexao.php";
 
 <head>
     <meta charset="UTF-8">
-    <title>Preenchendo campo SELECT com informações do banco de dados</title>
+    <title>UPDATE no MySQL</title>
     <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 
 <body>
-    <h2>Cadastrar usuários</h2>
+    <h2>Editar usuário</h2>
 
     <?php
-    //Atribuindo link na QUERY e substituir o link pelo valor com bindParam (RECOMENDADO)
-    /*$nome = "Mary1";
-    $email = "mary@gmail.com";
-    $senha = 123456;
-    $sists_usuario_id = 3;
-    $niveis_acesso_id = 3;
+    $id = 1;
+    $nome = "Cesar 1";
+    $email = "leohenricardoso@gmail.com";
 
+    $query_usuario = "UPDATE usuarios SET nome=:nome, email=:email WHERE id=:id";
+    $up_usuario = $conn->prepare($query_usuario);
+    $up_usuario->bindParam(':nome', $nome, PDO::PARAM_STR);
+    $up_usuario->bindParam(':email', $email, PDO::PARAM_STR);
+    $up_usuario->bindParam(':id', $id, PDO::PARAM_STR);
 
-*/
-    //filtra dados enviados através de um formulário
-    $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-
-    if (!empty($dados['SendCadUsuario'])) {
-
-        try {
-            $query_usuario = "INSERT INTO usuarios (nome, email, senha, sists_usuario_id, niveis_acesso_id, created) VALUES (:nome, :email, :senha, :sists_usuario_id, :niveis_acesso_id, NOW())";
-            $cad_usuario = $conn->prepare($query_usuario);
-            $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
-            $cad_usuario->bindParam(':email', $dados['email']);
-            //criptografar senha
-            $senha_cript = password_hash($dados['senha'], PASSWORD_DEFAULT);
-            $cad_usuario->bindParam(':senha', $senha_cript);
-            $cad_usuario->bindParam(':sists_usuario_id', $dados['sists_usuario_id'], PDO::PARAM_INT);
-            $cad_usuario->bindParam(':niveis_acesso_id', $dados['niveis_acesso_id'], PDO::PARAM_INT);
-            $cad_usuario->execute();
-
-            if ($cad_usuario->rowCount()) {
-                echo "Usuário cadastrado com sucesso!";
-                //destroi os dados mantidos no formulário
-                unset($dados);
-            } else {
-                echo "ERRO: Usuário não cadastrado";
-            }
-        } catch (PDOException $erro) {
-            echo "ERRO: Usuário não cadastrado.";
-            //echo "ERRO: Usuário não cadastrado. Erro gerado: " . $erro->getMessage() . "<br>";
-        }
+    if ($up_usuario->execute()) {
+        echo "Usuario editado com sucesso";
+    } else {
+        echo "ERRO: Usuario não editado";
     }
     ?>
-    <form method="POST" action="">
-        <label>Nome:</label>
-        <!-- Mantendo dados no formulário:  -->
-        <?php
-        $nome = "";
-        if (isset($dados['nome'])) {
-            $nome = $dados['nome'];
-        }
-        ?>
-        <input type="text" name="nome" placeholder="Nome completo" value="<?php echo $nome; ?>" required /><br><br>
-        <label>E-mail:</label>
-        <?php
-        $email = "";
-        if (isset($dados['email'])) {
-            $email = $dados['email'];
-        }
-        ?>
-        <input type="email" name="email" placeholder="E-mail do usuário" value="<?php echo $email; ?>" required /><br><br>
-        <label>Senha:</label>
-        <?php
-        $senha = "";
-        if (isset($dados['senha'])) {
-            $senha = $dados['senha'];
-        }
-        ?>
-        <input type="password" name="senha" placeholder="Senha do usuário" value="<?php echo $senha; ?>" required /><br><br>
-
-        <label>Situação:</label>
-        <?php
-        $query_sists_usuario = "SELECT id, nome FROM sists_usuarios ORDER BY nome ASC";
-        $result_sists_usuario = $conn->prepare($query_sists_usuario);
-        $result_sists_usuario->execute();
-        ?>
-        <!-- Caixa de seleção -->
-        <select name="sists_usuario_id" required>
-            <!-- Define uma única opção dentro de um elemento da caixa de seleção que podeá ser escolhida -->
-            <option value="">Selecione</option>
-            <?php
-            //para ler mais de um registro
-            while ($row_sist_usuario = $result_sists_usuario->fetch(PDO::FETCH_ASSOC)) {
-                $select_sit_usuario = "";
-                //Verifica se o existe um valor nos dados do formulário e se uma opçao foi selecionada previamente
-                if (isset($dados['sists_usuario_id']) and ($dados['sists_usuario_id'] == $row_sist_usuario['id'])) {
-                    //aparece a opção pré-selecionada no formulário caso a condição seja verdadeira
-                    $select_sit_usuario = "selected";
-                }
-                //Obtem o valor do registro atual tornando-a pré-selecionada
-                echo "<option value=' " . $row_sist_usuario['id'] . "' $select_sit_usuario>" . $row_sist_usuario['nome'] . " </option>";
-            }
-            ?>
-        </select>
-        <br><br>
-
-        <label>Nível de acecsso:</label>
-        <?php
-        $query_niveis_acesso = "SELECT id, nome FROM niveis_acessos ORDER BY nome ASC";
-        $result_niveis_acesso = $conn->prepare($query_niveis_acesso);
-        $result_niveis_acesso->execute();
-        ?>
-        <select name="niveis_acesso_id" required>
-            <option value="">Selecione</option>
-            <?php
-            while ($row_niveis_acesso = $result_niveis_acesso->fetch(PDO::FETCH_ASSOC)) {
-                $select_niveis_acesso = "";
-                if (isset($dados['niveis_acesso_id']) and ($dados['niveis_acesso_id'] == $row_niveis_acesso['id'])) {
-                    $select_niveis_acesso = "selected";
-                }
-                echo "<option value=' " . $row_niveis_acesso['id'] . "' $select_niveis_acesso>" . $row_niveis_acesso['nome'] . " </option>";
-            }
-            ?>
-        </select>
-        <br><br>
-        <input type="submit" value="Cadastrar" name="SendCadUsuario" />
 
     </form>
 
