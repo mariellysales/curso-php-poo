6d4223e3d76f851869672198ef2e16234616f555
Marielly Sales <mariellysales01@icloud.com>
2023-07-28 15:25:23 -0300

    RIGHT JOIN

--- index.php
+++ index.php
@@ -6,26 +6,27 @@ include_once "conexao.php";
 
 <head>
     <meta charset="UTF-8">
-    <title>LEFT JOIN</title>
+    <title>RIGHT JOIN</title>
     <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 
 <body>
     <h2>Listar usuários</h2>
     <?php
-    $query_usuarios = "SELECT usr.id AS id_usr, usr.nome AS nome_usr, usr.email, cont.telefone, cont.celular, ende.logradouro, ende.numero, ende.bairro, ende.cidade FROM usuarios as usr LEFT JOIN contatos AS cont ON cont.usuario_id=usr.id LEFT JOIN enderecos AS ende ON ende.usuario_id=usr.id ORDER BY usr.id DESC LIMIT 40";
+    //Retorna dados da tabela principal mesmo que a mesma não tenha valor, desde que tenha na tabela secundária
+    $query_usuarios = "SELECT cont.id AS id_cont, cont.telefone, cont.celular, usr.id AS id_usr, usr.nome, usr.email FROM contatos AS cont RIGHT JOIN usuarios AS usr ON usr.id=cont.usuario_id ORDER BY cont.id DESC LIMIT 40";
     $result_usuarios = $conn->prepare($query_usuarios);
     $result_usuarios->execute();
 
     while ($row_usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)) {
         //var_dump($row_usuario);
         extract($row_usuario);
-        echo "ID do usuário: $id_usr <br>";
-        echo "Nome do usuário: $nome_usr <br>";
-        echo "E-mail: $email <br>";
+        echo "ID do contato: $id_cont <br>";
         echo "Telefone do usuário: $telefone <br>";
         echo "Celular do usuário: $celular <br>";
-        echo "Endereço: $logradouro, $numero, $bairro - $cidade <br>";
+        echo "ID do usuário: $id_usr <br>";
+        echo "Nome do usuário: $nome <br>";
+        echo "E-mail: $email <br>";
         echo "<hr>";
     }
     ?>
